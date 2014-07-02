6b4b20ad86776a4e72973ec91cf489126e54bd7b
Péter Gombos <pego@bdo.no>
2014-07-02 11:03:16 +0200

    Add strategy as dependency

--- Source/backstage.js
+++ Source/backstage.js
@@ -1,10 +1,10 @@
 var angular = require('angular');
 var ui_router = require('angular-ui-router');
 
-var user = require('user')
-		services = require('services');
+var user = require('user'),
+		strategi = require('strategi');
 
-var backstage = angular.module('backstage', ['ui.router', 'user', 'services']);
+var backstage = angular.module('backstage', ['ui.router', 'user', 'strategi']);
 
 backstage.directive('moduleRenderer', require('./app/directives/renderer.js'));
 backstage.config(require('./app/routes.js'));
